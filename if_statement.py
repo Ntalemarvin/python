8bec996cc08de15f1a6059737afea29cf8f5e183
Ntalemarvin <makvelimarvin@gmail.com>
2021-09-01 19:39:44 +0300

    added an if_statement.py

---
+++ if_statement.py
@@ -0,0 +1,12 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's hot")
+    print('Take plenty of water')
+elif is_cold:
+    print("It's cold")
+    print('wear warm clothes')
+else:
+    print("It's a lovely day")
+print('Have a lovely day')
