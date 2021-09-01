af49e6f846a897983ecce299cb1ac6325534e2fe
Ntalemarvin <makvelimarvin@gmail.com>
2021-09-01 20:21:07 +0300

    updated my if_statment.py file

--- if_statement.py
+++ if_statement.py
@@ -10,3 +10,23 @@ elif is_cold:
 else:
     print("It's a lovely day")
 print('Have a lovely day')
+
+#write the code where:
+'''
+price of a house is $1M
+if buyer has good credit,
+    they need to put down 10%
+otherwise
+    they need to put down 20%
+print the down the payment
+
+'''
+
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+print(f"Down payment: ${down_payment}")
