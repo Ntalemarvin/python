cb0437a136963d8cea0c6ef78c57dc210828f804
Ntalemarvin <makvelimarvin@gmail.com>
2021-09-02 19:44:23 +0300

    added both games using while loop

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+print('Done')
+
+x = 1
+while x <= 5:
+    print('woow')
+    x += 1
+print('woow is done')
+
+y = 1
+while y <= 5:
+    print('*' * y)
+    y +=1
+print("i'm out of this dem * ")
